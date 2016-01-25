f5290b47184101399651ab898f117d29b9a32472
aholovenko <aholovenko@hotmail.com>
2016-01-25 20:38:43 +0200

    SetUpForJS

--- js/main.js
+++ js/main.js
@@ -2,11 +2,11 @@
  * Created by 1 on 20.01.2016.
  */
 //You	should	wrap	all	your	jQuery	code
-$(function () {
+//$(function () {
 //This	function	will	be	called
 //	after	the	DOM	was	loaded	and	ready
 //Here	we	can	place	all	jQuery	Code
-    $('input').click(function () {
-        $(this).css("font-size", "30px");
-    });
-});
+ //   $('input').click(function () {
+ //       $(this).css("font-size", "30px");
+ //   });
+//});
